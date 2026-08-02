2a0db5cbad283e92d9ecef07b1a87e691fbafd08
arindammohanty <arindammohanty100@gmail.com>
2026-08-02 16:43:07 +0530

    feat: add Vercel Analytics

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -19,5 +20,6 @@ if ('serviceWorker' in navigator) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
